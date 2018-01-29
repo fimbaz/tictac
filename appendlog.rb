aa5f5728498a68010eb8e114dcaf3678cceec164
Ben C Lewis <benjamin.connah.lewis@gmail.com>
2018-01-29 03:53:34 +0000

    moving internal data into refactorable functions

--- appendlog.rb
+++ appendlog.rb
@@ -5,32 +5,33 @@ require 'json'
 require_relative 'tictac'
 module TicTac
   class AppendLog
-    attr_reader :config
 
     def initialize(initial_state: nil, config: DefaultConfig)
       @config = config
-      @pkey=OpenSSL::PKey::RSA.new(File.read(config.private_key))
-      @log=[]
+
       if initial_state.nil?
         obj={last_log: nil, payload: ''}
-        @log.push(signed_obj(obj))
+        push(insert_signed_obj(obj))
       else
         #TODO verification of chain so far
       end
       
     end
+
     def verify_entry(log_entry)
       
     end
+
     def new_entry(obj)
       last_payload=JSON.parse(%x(ipfs cat #{@log.last}),symbolize_names: true)
-      obj[:last_log]=@log.last
-      sobj=signed_obj(obj)
-      @log.push(sobj).last
+      obj[:last_log]=@last_entry
+      sobj=insert_signed_obj(obj)
+      push(sobj)
     end
-    def signed_obj(obj)
+
+    def insert_signed_obj(obj)
       json_obj=JSON.dump(obj)
-      signature=Base64.strict_encode64(@pkey.sign(OpenSSL::Digest::SHA256.new,json_obj))
+      signature=Base64.strict_encode64(pkey.sign(OpenSSL::Digest::SHA256.new,json_obj))
       signed_obj=JSON.dump(payload: Base64.strict_encode64(json_obj),
                   signature: signature,
                   signer: File.read(config.public_key)
@@ -39,6 +40,24 @@ module TicTac
     end
 
     private
+
+    attr_reader :config
+
+    attr_accessor :last_entry
+
+    def log
+      @log ||= []
+    end
+
+    def pkey
+      @pkey ||= OpenSSL::PKey::RSA.new(File.read(config.private_key))
+    end
+
+    def push(obj)
+      last_entry = obj
+      log.push(obj) if config.store_log
+      obj
+    end
 
     def ipfs_add(str)
       Open3.popen3("ipfs add -Q") do | i,o,e|
--- tictac.rb
+++ tictac.rb
@@ -1,6 +1,6 @@
 require 'fileutils'
 module TicTac
-  Config = Struct.new(:ipfs_path, :tictac_dir, :private_key, :public_key, :empty_log)
+  Config = Struct.new(:ipfs_path, :tictac_dir, :private_key, :public_key, :empty_log, :store_log)
 
   DefaultConfig = begin
     ipfs_path = ENV['IPFS_PATH'] ? ENV['IPFS_PATH'] : File.absolute_path("#{ENV['HOME']}/.ipfs")
@@ -9,7 +9,8 @@ module TicTac
       "#{ipfs_path}/tictac",
       "#{ipfs_path}/tictac/self.pem",
       "#{ipfs_path}/tictac/self.pub",
-      "QmbJWAESqCsf4RFCqEY7jecCashj8usXiyDNfKtZCwwzGb"
+      "QmbJWAESqCsf4RFCqEY7jecCashj8usXiyDNfKtZCwwzGb",
+      true
     )
   end
 
